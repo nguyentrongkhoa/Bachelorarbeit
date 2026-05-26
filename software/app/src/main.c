8833186529a435b9f88fad6553c1efe8be3d4ddb
Tom <khoa190602@gmail.com>
2026-05-26 11:58:43 +0200

    Source code of blinky

--- software/app/src/main.c
+++ software/app/src/main.c
@@ -5,10 +5,19 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
 int main(void)
 {
-	
+	static const struct gpio_dt_spec status_led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+	if (!gpio_is_ready_dt(&status_led)) {
+		return -ENODEV;
+	}
+	gpio_pin_configure_dt(&status_led, GPIO_OUTPUT_ACTIVE);
+	while (1) {
+		gpio_pin_toggle_dt(&status_led);
+		k_msleep(1000);
+	}
 }
 
