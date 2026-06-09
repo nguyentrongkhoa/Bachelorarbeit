b10ae7bd6d8a0a35763d94b19ba5c37fcd7bbdd0
Tom <khoa190602@gmail.com>
2026-06-09 11:40:01 +0200

    Corrected DevEUI and AppKey

--- software/app/src/main.c
+++ software/app/src/main.c
@@ -6,7 +6,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <errno.h>
-
 #include <zephyr/sys/util.h>
 #include <zephyr/sys/printk.h>
 
@@ -14,6 +13,8 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/hwinfo.h>
+
+#include <zephyr/lorawan/lorawan.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -25,14 +26,14 @@
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
-uint8_t hw_id[12]; // 12 bytes or 96 bits
 uint8_t dev_eui64[8]; // 8 bytes or 64 bits
 char dev_eui64_str[17]; // 16 characters for EUI-64 in hex + null terminator
 int ret; // return status
 
-#define LORAWAN_DEV_EUI  {0x68, 0x76, 0x62, 0x52, 0x48, 0x01, 0x50, 0x38}
+#define LORAWAN_DEV_EUI  {0x00, 0x80, 0xE1, 0x15, 0x06, 0x92, 0x66, 0x73}
 #define LORAWAN_JOIN_EUI {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00} // default for TTN
-#define LORAWAN_APP_KEY  {0x04, 0x2F, 0xF6, 0x86, 0x22, 0xE9, 0x3B, 0xCF, 0x58, 0x8E, 0x5A, 0x6E, 0xCD, 0xA2, 0xF8, 0x3C}
+#define LORAWAN_APP_KEY  {0x3A, 0xC3, 0xC1, 0x95, 0x56, 0x82, 0x07, 0x3B, \ 
+	                      0x3C, 0xD0, 0xED, 0xAD, 0xB6, 0xFF, 0x1B, 0xDE} // obtained on TTN when registering a new end device
 
 int main(void) {
 	k_msleep(5000);
@@ -42,14 +43,13 @@ int main(void) {
 	lora_init();
 	rf_switch_init();
 	// ---------------------------
-
-	// Manually generate 64-bit DevEUI from 96-bit hardware ID to connect to LoRaWAN network servers
-	hwinfo_get_device_id(hw_id, sizeof(hw_id)); 
-	//calculate 64-bit DevEUI using standard EUI-64 generation method (XOR first 8 bytes with last 4 bytes)
-	for (int i = 0; i < 8; i++) {
-		dev_eui64[i] = hw_id[i] ^ hw_id[i + 4]; 
+	// retrieve 64-bit/8-byte DevEUI to connect to TTN
+	ret = hwinfo_get_device_eui64(dev_eui64);
+	if (ret < 0) {
+		LOG_ERR("Cannot retrieve 64-bit DevEUI\n");
+		return -1; // halt main completely
 	}
-	// Convert the EUI-64 to a hexadecimal string for easier display and use
+	// Convert the DevEUI-64 to a hexadecimal string for easier display and use
 	for (int i = 0; i < 8; i++) {
 		snprintf(&dev_eui64_str[i * 2], 3, "%02X", dev_eui64[i]);
 	}
